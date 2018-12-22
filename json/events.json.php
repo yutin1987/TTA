f8d3ccece273251d941be67ad18d3ec2b7d1b556
quinnai9287 <keira0930@gmail.com>
2018-12-22 20:56:44 +0800

    rwd

---
+++ json/events.json.php
@@ -0,0 +1,77 @@
+{
+	"success": 1,
+	"result": [
+		{
+			"id": "293",
+			"title": "BE Meetup",
+			"url": "javascript:;",
+			"class": "",
+			"start": "1535812200000",
+			"end":   "1535823000000"
+		},
+		{
+			"id": "256",
+			"title": "BE Meetup",
+			"url": "javascript:;",
+			"class": "",
+			"start": "1536003000000",
+			"end":   "1536010200000"
+		},
+		{
+			"id": "276",
+			"title": "IAPS X SEA Anchor",
+			"url": "javascript:;",
+			"class": "",
+			"start": "1536175800000",
+			"end":   "1536183000000"
+		},
+		{
+			"id": "294",
+			"title": "Zino Delegation Ev",
+			"url": "javascript:;",
+			"class": "",
+			"start": "1536676200000",
+			"end":   "1536683400000"
+		},
+		{
+			"id": "297",
+			"title": "FITI Meeting",
+			"url": "#",
+			"class": "",
+			"start": "1537039800000",
+			"end":   "1537039800000"
+		},
+		{
+			"id": "54",
+			"title": "Soft-landing 2018",
+			"url": "javascript:;",
+			"class": "",
+			"start": "1537198200000",
+			"end":   "1537198200000"
+		},
+		{
+			"id": "532",
+			"title": "IAPS User Interview",
+			"url": "javascript:;",
+			"class": "",
+			"start": "1537374600000",
+			"end":   "1537381800000"
+		},
+		{
+			"id": "548",
+			"title": "TTA x TIEV Bootca",
+			"url": "javascript:;",
+			"class": "",
+			"start": "1537381800000",
+			"end":   "1537389000000"
+		},
+		{
+			"id": "295",
+			"title": "Meet TTA",
+			"url": "javascript:;",
+			"class": "",
+			"start": "1537551000000",
+			"end":   "1537558200000"
+		}
+	]
+}
